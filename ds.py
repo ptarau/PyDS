129b48b783c2545500eeb1f7b7c4b794a5d2aa4a
Paul Tarau <ptarau@gmail.com>
2019-08-28 17:46:02 -0500

    updates to ds.py

--- ds.py
+++ ds.py
@@ -6,11 +6,17 @@ a.append(2)
 
 print('a:',a)
 
-b=[3,4,5]
+b=[3,4,5,6,7]
 
+print(b[0])
 c=a+b
 
 print('c:',c)
+
+# tuples
+
+t=(1,2,3)
+tt=t+t
 
 # sets
 
@@ -18,13 +24,15 @@ xs=set()
 
 xs.add(1)
 xs.add(2)
+xs.add(3)
+xs.add(4)
 
 ys={3,4,5}
 
 print('xs',xs)
 print('ys',ys)
 
-def union(xs,y) :
+def union(xs,ys) :
   def f() :
     for x in xs :
       yield x
@@ -32,6 +40,30 @@ def union(xs,y) :
       yield y
   return set(f())
 
+def intersect(xs,ys) :
+  def f() :
+    for x in xs :
+      if x in ys :
+        yield x
+  return set(f())
+
+zs = set(range(2,7))
+
 print('union:',union(xs,ys))
+
+print('intersect:',intersect(xs,zs))
+
+# dicts
+
+d=dict()
+
+
+key=('good','bad')
+
+d['hello']='bye'
+d[42]='meaning of life'
+d[key]='maybe'
+
+dd={1:'one',2:'two'}
 
 
