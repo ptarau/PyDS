bd1920292165c1442d04e952adca1c2ce4c1d61f
Paul Tarau <ptarau@gmail.com>
2019-09-16 13:18:20 -0500

    updated graphviz for trees

--- graphvizTree.py
+++ graphvizTree.py
@@ -1,12 +1,34 @@
 from graphviz import Digraph
 
+def decorateSimple(t) :
+  i=0
+  def st(t) :
+    nonlocal i
+    if t :
+      j = i
+      for s in t:
+        i += 1
+        yield (str(j),str(i))
+        for e in st(s): yield e
+
+  for e in st(t) : yield e
+
+def showSimple(t) :
+  g=Digraph()
+  for e in decorateSimple(t) :
+    f,t=e
+    #g.node(f,'*')
+    #g.node(t,'*')
+    g.edge(f,t)
+  g.view()
+
 def showTree(t) :
   g=Digraph()
   i=0
   
   def label(x) : 
     if isinstance(x,tuple) :
-      return x[0]
+      return str(x[0])
     else :
       return str(x)
    
@@ -19,7 +41,6 @@ def showTree(t) :
         
   def st(a) :   
     nonlocal i
-  
     if isinstance(a,tuple) :
       op=a[0]
       i0=i
@@ -30,6 +51,7 @@ def showTree(t) :
         st(x)
      
   st(t)
+  # print(g)
   g.view()   
   
 
@@ -43,8 +65,9 @@ def t1() :
   g.node('3','c')
   g.edge('1','3')
   g.view()
+  g.clear()
 
-def t2() :
+def t1a() :
   g=Digraph()
   g.edge('1','2')
   g.edge('1','3')
@@ -52,3 +75,41 @@ def t2() :
   g.edge('3','1')
   g.view() 
   
+def t2() :
+  t=('l', ('l', ('a', ('a', 0, ('l', 3)), ('a', ('l', 0), 0))))
+  print(t)
+  showTree(t)
+  
+def l(x) : ('l',x)
+def a(x,y) : ('a',x,y)
+
+def t3() :
+   t=l(a(l(a(l(0),l(1))),a(0,0)))
+   print(t)
+   showTree(t)
+   
+def s1() :
+  return  list(decorateSimple(((),((),(),()),(),(((),),))))
+
+def s2() :
+  showSimple(((),((),(),()),(),(((),),)))
+  
+# test on multiple trees  
+
+# defines lambda node
+def l(x) : return ('l',x)
+
+# defines app;lication node
+def a(x,y) : return ('a',x,y)
+
+def test(i) :
+  ts=[
+    l(a(a(1,a(l(l(0)),1)),1)),
+    l(l(l(l(a(0,a(1,l(l(a(0,a(2,0)))))))))),
+    l(l(l(a(a(0,a(1,a(a(l(l(0)),0),2))),1)))),
+    l(l(l(a(l(1),a(l(a(1,0)),a(a(a(2,0),0),0)))))),
+    l(l(l(a(l(a(2,a(a(l(0),l(a(1,a(1,a(1,l(1)))))),l(0)))),a(2,0)))))
+  ]
+  t=ts[i]
+  print(i,'==>',t)
+  showTree(t)
