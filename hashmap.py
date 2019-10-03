4bba7d8c53d00ce762609e295f3655ec6230fa11
Paul Tarau <ptarau@gmail.com>
2019-10-02 19:09:07 -0500

    finished hashmap

--- hashmap.py
+++ hashmap.py
@@ -7,36 +7,70 @@ class hashmap() :
   def add(self,k,v):
     h=hash(k) % len(self.H)
     vs=self.H[h]
-    found=False
+    if not vs : vs=[(k,v)]
+    else :
+      found=False
+      for i in range(len(vs)) :
+        if vs[i][0]==k :
+          found=True
+          vs[i][1]=v
+          break
+      if not found :
+        vs.append((k,v))
+    self.H[h]=vs
+
+  def get(self,k) :
+    h = hash(k) % len(self.H)
+    vs = self.H[h]
+    for i in range(len(vs)):
+      if vs[i][0] == k: return vs[i][1]
+    return None
+
+  def remove(self,k) :
+    h = hash(k) % len(self.H)
+    vs = self.H[h]
+    if not vs: return
+    where=-1
     for i in range(len(vs)) :
-       if vs[i][0]==k :
-         found=True
-         vs[i][1]=v
-         break
-    if not found :
-      vs.append((k,v))
+      if vs[i][0]==k :
+        where=i
+        break
+    if where >= 0 :
+      vs.pop(where)
+
+
 
 
   def __str__(self):
-    return str(self.H)
-    '''
-    buf=[]
-    for kv in H :
-      if kv :
-        for k,v in H[k] :
-          buf.append((str(k),str(v)))
-    '''
-
-
-
-
-
-
+    #return str(self.H)
+    buf=['[\n']
+    i=0
+    for kvs in self.H :
+      if kvs :
+        buf.append(str(i) + ":")
+        for kv in kvs :
+          buf.append(str(kv))
+        buf.append("\n")
+      i+=1
+    buf.append(']')
+    return "".join(buf)
 
 def t1() :
   d=hashmap()
   d.add('hello','Joe')
   d.add("bye",'Bill')
   d.add("howdy",'Mary')
+  d.add("cheers",'Mike')
+  d.add("so long", 'Jill')
   print(d)
+  print(d.get('howdy'))
+  d.remove('noShuchKey')
+  d.remove('cheers')
+  print(d)
+  print(d.get('cheers'))
+  return(d)
 
+t1()
+
+# todo: make the hashMap grow and shrink
+# todo: make it support Python [] syntax for get, add
