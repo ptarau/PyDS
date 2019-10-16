55af4eb490d55bef4be16f5e0121530b58e5bf36
Paul Tarau <ptarau@gmail.com>
2019-10-16 12:24:59 -0500

    added graphs.py, including graphviz

--- graphs.py
+++ graphs.py
@@ -2,18 +2,19 @@ import networkx as nx
 from graphviz import Digraph
 
 # displays graph with graphviz
-def showDot(dot,show=True,file_name='graph.gv')  :
+def show(dot,show=True,file_name='graph.gv')  :
   dot.render(file_name, view=show) 
   
 def showGraph(g) :
   dot=Digraph()
   for e in g.edges():
+    print(e)
     f,t=e
-    dot.edge(str(f), str(t),label='x')
-    showDot(dot)
+    dot.edge(str(f), str(t),label='')
+  print(dot.source)
+  show(dot)
 
-        
-def t0() :
+def t1() :
   # edges
   es = [(1,2),(2,3),(1,3),(3,4),(4,5),(5,6),(4,6),(2,5),(1,6)]
   G=nx.Graph()
