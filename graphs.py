c7727d77283fbcf37075609cc80d6a48216b15ae
Paul Tarau <ptarau@gmail.com>
2019-10-18 09:06:19 -0500

    no message

--- graphs.py
+++ graphs.py
@@ -1,6 +1,32 @@
 import networkx as nx
 from graphviz import Digraph
 
+class DGraph :
+  def __init__(self):
+    self.d=dict()
+
+  def add_edge(self,e):
+    f,t=e
+    vs=self.d.get(f)
+    if not vs :
+      self.d[f]={t}
+    else :
+      vs.add(t)
+
+  def add_edges_from(self,es):
+    for e in es :
+      self.add_edge(e)
+
+  def __repr__(self):
+    return self.d.__repr__()
+
+def g1() :
+  # edges
+  es = [(1,2),(2,3),(1,3),(3,4),(4,5),(5,6),(4,6),(2,5),(1,6)]
+  G=DGraph()
+  G.add_edges_from(es)
+  print('graph =',G)
+
 # displays graph with graphviz
 def show(dot,show=True,file_name='graph.gv')  :
   dot.render(file_name, view=show) 
