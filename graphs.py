b4bb1e2fb928a23c169864cfc54fcc9b39bf9945
Paul Tarau <ptarau@gmail.com>
2019-10-18 16:24:11 -0500

    more on graphs

--- graphs.py
+++ graphs.py
@@ -1,12 +1,21 @@
 import networkx as nx
-from graphviz import Digraph
+import graphviz as gv
 
 class DGraph :
   def __init__(self):
     self.d=dict()
 
+  def clear(self) :
+    self.d = dict()
+
+  def add_node(self,n):
+    if not self.d.get(n) :
+      self.d[n]=set()
+
   def add_edge(self,e):
     f,t=e
+    self.add_node(f)
+    self.add_node(t)
     vs=self.d.get(f)
     if not vs :
       self.d[f]={t}
@@ -17,14 +26,38 @@ class DGraph :
     for e in es :
       self.add_edge(e)
 
+  def edges(self):
+    for f in self.d :
+      for t in self.d[f] :
+        yield (f,t)
+
+  def number_of_nodes(self) :
+    return len(self.d)
+
   def __repr__(self):
     return self.d.__repr__()
+
+  def show(self):
+    dot = gv.Digraph()
+    for e in self.edges():
+      #print(e)
+      f, t = e
+      dot.edge(str(f), str(t), label='')
+    #print(dot.source)
+    show(dot)
+
+
 
 def g1() :
   # edges
   es = [(1,2),(2,3),(1,3),(3,4),(4,5),(5,6),(4,6),(2,5),(1,6)]
   G=DGraph()
   G.add_edges_from(es)
+  G.add_node(9)
+  for e in G.edges() : print('edge',e)
+  print('nodes',G.number_of_nodes())
+  G.show()
+
   print('graph =',G)
 
 # displays graph with graphviz
@@ -32,7 +65,7 @@ def show(dot,show=True,file_name='graph.gv')  :
   dot.render(file_name, view=show) 
   
 def showGraph(g) :
-  dot=Digraph()
+  dot=gv.Digraph()
   for e in g.edges():
     print(e)
     f,t=e
