4be3fb0d935ba4b13481e3bd37b0091cce5c1f23
Paul Tarau <ptarau@gmail.com>
2019-10-25 21:21:13 -0500

    added iterative deepening graph traversal

--- nxgraphs.py
+++ nxgraphs.py
@@ -1,24 +1,25 @@
 import networkx as nx
 import graphviz as gv
-import networkx.algorithms.traversal.depth_first_search as algs
 
 class digraph(nx.DiGraph) :
 
   def rand(self):
-    er = nx.dense_gnm_random_graph(24, 27)
+    er = nx.dense_gnm_random_graph(12, 12)
     for e in er.edges() :
       f,t=e
       self.add_edge(f,t)
-    er = nx.dense_gnm_random_graph(24, 7)
+    er = nx.dense_gnm_random_graph(12, 12)
     for e in er.edges() :
       f,t=e
       self.add_edge(t,f)
+    if 0 not in self.nodes(): self.add_edge(0, 1)
 
     return self
 
   def show(self):
     return show(self)
 
+# depth first traversal
 def df_nodes(g,source):
   visited=set()
   def visit(node) :
@@ -31,6 +32,26 @@ def df_nodes(g,source):
   for n in visit(source) :
     yield n
 
+# iterative deepening traversal
+def id_nodes(g,source,max_depth=None):
+  if not max_depth :
+    max_depth=g.number_of_nodes()
+  seen=set()
+  def visit(node,fuel) :
+    #print('trace',visited)
+    if node in visited or not fuel : return
+    visited.add(node)
+    if node not in seen : yield node
+    seen.add(node)
+    for child in  g[node] :
+      for n in visit(child,fuel-1) :
+        yield n
+  for fuel in range(0,max_depth+1) :
+    visited=set()
+    for n in visit(source,fuel) :
+      yield n
+
+
 def show(g):
   dot = gv.Digraph()
   for e in g.edges():
@@ -39,7 +60,6 @@ def show(g):
     #print(dot.source)
   dot.render('graph.gv', view=True)
   return g
-
 
 def t1() :
   # edges
@@ -53,11 +73,19 @@ def t2() :
 
 def t3() :
   g=digraph().rand().show()
-  ns=algs.dfs_preorder_nodes(g,source=0)
+  ns=nx.dfs_preorder_nodes(g,source=0)
   print('networx',list(ns))
   print('ours   ',list(df_nodes(g,0)))
 
-t3()
+def t4() :
+  g=digraph().rand().show()
+  es=nx.bfs_edges(g, 0)
+  ns=[0]+list(map(lambda b:b[1], es))
+  print('networx',list(ns))
+  print('ours   ',list(id_nodes(g,0)))
 
 
-
+#t1()
+#t2()
+#t3()
+t4()
