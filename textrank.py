cd126d1ebd0d89825c004269e14f0ea3458eff40
Paul Tarau <ptarau@gmail.com>
2019-11-18 17:45:26 -0600

    sketch of textrank

--- textrank.py
+++ textrank.py
@@ -1,12 +1,11 @@
 # ranking a document for most important sentences
-
 import nltk
 import networkx as nx
+import heapq
 
-def tokenize(s) :
-  s = s.replace('.', ' .')
-  ws = s.split()
-  return ws
+def good_word(wt) :
+  t=wt[1]
+  return t[0] in "NJV"
 
 def digest(doc) :
   text=[]
@@ -16,32 +15,45 @@ def digest(doc) :
       text.append(line+" ")
   text="".join(text)
   sents=nltk.sent_tokenize(text)
-  wss=map(tokenize,sents)
-  return wss
+  wss=map(nltk.word_tokenize,sents)
+  goodWordsSets=[]
+  for ws in wss :
+    ts=set(w for (w,t) in nltk.pos_tag(ws) if good_word((w,t)))
+    goodWordsSets.append(ts)
+  return goodWordsSets,sents
 
   #print(list(wss))
 
 def build_intersection_graph(wss) :
   g=nx.DiGraph()
-  for s in wss :
-    print(s)
+  m=len(wss)
+  for i in range(0,m) :
+    for j in range(i,m) :
+      if wss[i].intersection(wss[j]) :
+        g.add_edge(i,j)
+  return g
+
+def best_sents(g,k) :
+  d=nx.pagerank(g)
+  ranked=[]
+  for (w,r) in d.items() :
+    heapq.heappush(ranked,(r,w))
+  for _ in range(k) :
+    (r,i) = heapq.heappop(ranked)
+    yield i
 
 
-def t1() :
-  r=nltk.sent_tokenize("The cat sits on the mat. The dog barks.")
-  for s in r :
-    s=s.replace('.',' .')
-    ws=s.split()
-    print(ws)
-
-
-def t2() :
-  wss=digest('doc.txt')
-  build_intersection_graph(wss)
-  # if intersection has elements
-  # connect latest sentence to earler
+def summarize(fname='us_constitution.txt') :
+  wss,sents=digest(fname)
+  g=build_intersection_graph(wss)
+  for i in best_sents(g,5) :
+    yield i,sents[i]
   
 
 #print(list(r))
 
-t2()
+def go() :
+  for ns in summarize() :
+    print(ns)
+
+go()
